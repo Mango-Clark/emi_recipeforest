381ba3dd799552f8fca91311af1335a1ac8e47da
Mango-Clark <100078337+Mango-Clark@users.noreply.github.com>
2026-08-23 08:15:48 +0200

    fix(config): use Java 17 clamp API

--- common/src/main/java/io/github/mango_clark/emirecipeforest/mixin/ConfigScreenMixin.java
+++ common/src/main/java/io/github/mango_clark/emirecipeforest/mixin/ConfigScreenMixin.java
@@ -40,6 +40,7 @@ import net.minecraft.client.gui.screens.ConfirmScreen;
 import net.minecraft.client.gui.screens.Screen;
 import net.minecraft.client.gui.screens.inventory.tooltip.ClientTooltipComponent;
 import net.minecraft.network.chat.Component;
+import net.minecraft.util.Mth;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.Unique;
@@ -178,8 +179,8 @@ public abstract class ConfigScreenMixin extends Screen {
 
                     @Override
                     protected void setValue(IntGroup value) {
-                        int columns = Math.clamp(value.values.getInt(0), 1, 16);
-                        int rows = Math.clamp(value.values.getInt(1), 1, 8);
+                        int columns = Mth.clamp(value.values.getInt(0), 1, 16);
+                        int rows = Mth.clamp(value.values.getInt(1), 1, 8);
                         value.values.set(0, columns);
                         value.values.set(1, rows);
                         ForestBookmarks.setRootGridSize(columns, rows);
